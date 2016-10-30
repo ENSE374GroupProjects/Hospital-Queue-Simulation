92408c85f0fab76e772e269b7559e87b1839673d
Jonathon Wells <wellsjonathonr@gmail.com>
2016-10-30 14:48:35 -0600

    Added all getter functions for UserAccount, changed "medicalInfo" to "medicalHistory" and added "Symptom currentSymptom"

--- src/HospitalSimulation/UserAccount.java
+++ src/HospitalSimulation/UserAccount.java
@@ -4,44 +4,81 @@ import java.util.List;
 
 public class UserAccount
 {
-	/*Attributes*/
+	/*==================ATTRIBUTES=================*/
 	private int age;
 	private String name;
 	private Location currentLocation;
 	private Gender gender;
-	private String medicalInfo; // Change to List<Symptom> when implemented
-	
-	/*Methods*/
-	/*Default Constructor*/
+	private String medicalHistory; // Change to List<Symptom> when implemented
+	private Symptom currentSymptom;
+	/*=============================================*/
+
+	/*====================METHODS==================*/
+	/*-----------------CONSTRUCTORS----------------*/
+	/*Default*/
 	public UserAccount()
 	{
 		this.age = 0;
 		this.name = "Default User";
 		this.currentLocation = Location.DEFAULT;
 		this.gender = Gender.OTHER;
-		this.medicalInfo = "No pre-existing symptoms";
+		this.medicalHistory = "No medical history";
+		this.currentSymptom = Symptom.DEFAULT;
 		
 	}
 	
-	/*Constructor*/
-	public UserAccount(int age, String name, Location currentLocation, Gender gender, String medicalInfo)
+	/*Initializer*/
+	public UserAccount(int age, String name, Location currentLocation, Gender gender, String medicalHistory, Symptom currentSymptom)
 	{
 		this.age = age;
 		this.name = name;
 		this.currentLocation = currentLocation;
 		this.gender = gender;
-		this.medicalInfo = "No pre-existing symptoms";
+		this.medicalHistory = medicalHistory;
+		this.currentSymptom = currentSymptom;
 	}
-	
-	/*showSymptoms():void*/
-	/*Display the medicalInfo Symptoms*/
-	public void showSymptoms()
-	{
-		System.out.println("Pre-existing medical history: " + this.medicalInfo);
-	}
-	
+	/*---------------------------------------------*/
+
+	/*--------------------GETTERS------------------*/
 	public String getName()
 	{
 		return name;
 	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public Gender getGender()
+	{
+		return gender;
+	}
+	
+	public Location getLocation()
+	{
+		return currentLocation;
+	}
+	
+	public Symptom getCurrentSymptom()
+	{
+		return currentSymptom;
+	}
+	
+	public String getMedicalHistory()
+	{
+		return medicalHistory;
+	}
+	/*---------------------------------------------*/
+
+	/*showSymptoms()*/
+	/*Display the medicalInfo Symptoms*/
+	public void showMedicalHistory()
+	{
+		System.out.println("Pre-existing medical history: " + this.medicalHistory);
+	}
+	
+	/*=============================================*/
+	
+	
 }
