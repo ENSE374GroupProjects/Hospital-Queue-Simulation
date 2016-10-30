3e47b333adf94b837ce76f2d348c5ff5440f753e
Quinn Bast <Quinn.Bast@hotmail.com>
2016-10-30 13:20:04 -0600

    *Updated the Severity enumeration type

--- src/HospitalSimulation/Severity.java
+++ src/HospitalSimulation/Severity.java
@@ -1,5 +1,23 @@
 package HospitalSimulation;
 
 public enum Severity {
-
+	CRITICAL("Critical", 5), VERYURGENT("Very Urgent", 4), URGENT("Urgent", 3), STANDARD("Standard", 2), MINOR("Minor", 1);
+	private String gender;
+	private int severity;
+	
+	private Severity(String gender, int severity)
+	{
+		this.gender = gender;
+		this.severity = severity;
+	}
+	
+	public String toString()
+	{
+		return gender;
+	}
+	
+	public int toInt()
+	{
+		return severity;
+	}
 }
