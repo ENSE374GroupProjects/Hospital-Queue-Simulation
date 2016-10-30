21af82ffc23953fb03ed347a7d7a90f0d092d927
Tristan Heisler <tristanheisler@hotmail.ca>
2016-10-30 13:03:28 -0600

    Developed the Location enumerated type.

--- src/HospitalSimulation/Location.java
+++ src/HospitalSimulation/Location.java
@@ -1,5 +1,25 @@
 package HospitalSimulation;
 
-public enum Location {
-
+public enum Location 
+{
+	//Valid locations
+	NORTHWEST("northwest"), NORTH("north"), NORTHEAST("northeast"), 
+	WEST("west"), CENTRAL("central"), EAST("east"), 
+	SOUTHWEST("southwest"), SOUTH("south"), SOUTHEAST("southwest"),
+	DEFAULT("default location");
+	
+	//Local variable
+	private String location;
+	
+	//Constructor
+	private Location(String location)
+	{
+		this.location = location;
+	}
+	
+	//Converting the enum to a string
+	public String toString()
+	{
+		return this.location;
+	}
 }
