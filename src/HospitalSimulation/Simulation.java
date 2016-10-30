b19e276b3a0917c65b3244b8fc832aa6f3f375b8
Tristan Heisler <tristanheisler@hotmail.ca>
2016-10-30 11:54:43 -0600

    Created the Java project that will contain our hospital simulation.

---
+++ src/HospitalSimulation/Simulation.java
@@ -0,0 +1,10 @@
+package HospitalSimulation;
+
+public class Simulation 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This will become our hospital simulation app!");
+		System.out.println("By Tristan, Quinn and Jon");
+	}
+}
