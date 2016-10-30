a144e1e07922799821ac6326c212feee206a937f
Jonathon Wells <wellsjonathonr@gmail.com>
2016-10-30 12:24:00 -0600

    Created skeleton classes and enums for UserAccount, Gender, Severity, and Location

---
+++ src/HospitalSimulation/Gender.java
@@ -0,0 +1,5 @@
+package HospitalSimulation;
+
+public enum Gender {
+
+}
---
+++ src/HospitalSimulation/UserAccount.java
@@ -0,0 +1,26 @@
+package HospitalSimulation;
+
+import java.util.List;
+
+public class UserAccount
+{
+	/*Attributes*/
+	private int age;
+	private String name;
+	private Location currentLocation;
+	private Gender gender;
+	private List<Symptom> medicalInfo;
+	
+	/*Methods*/
+	/*Default Constructor*/
+	public UserAccount()
+	{
+		
+	}
+	
+	/*Constructor*/
+	public UserAccount(int age, String name, Location currentLocation, Gender gender, List<Symptom> medicalInfo)
+	{
+		
+	}
+}
