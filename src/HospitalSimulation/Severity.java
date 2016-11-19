b812c24e32761db202bbba70aa2fcc14fbf57140
Tristan Heisler <tristanheisler@hotmail.ca>
2016-11-19 11:11:27 -0600

    Resolved an improper variable name in Severity.java

--- src/HospitalSimulation/Severity.java
+++ src/HospitalSimulation/Severity.java
@@ -12,25 +12,25 @@ public enum Severity
 	DEFAULT("Default", 0);
 	
 	//Local variables
-	private String gender;
+	private String name;
 	private int severity;
 	
 	//Constructor
-	private Severity(String gender, int severity)
+	private Severity(String name, int severity)
 	{
-		this.gender = gender;
+		this.name = name;
 		this.severity = severity;
 	}
 	
 	//Return the enumerated type as a string
 	public String toString()
 	{
-		return gender;
+		return this.name;
 	}
 	
 	//Return the severity index of the enumerated type
 	public int getSeverityIndex()
 	{
-		return severity;
+		return this.severity;
 	}
 }
